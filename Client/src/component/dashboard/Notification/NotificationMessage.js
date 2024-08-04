175bdd309cee05dd7362c081b76a99b11f39503a
Nikhil Mishra <104861212+nikkk456@users.noreply.github.com>
2024-08-04 13:57:36 +0530

    Completed Notification

--- Client/src/component/dashboard/Notification/NotificationMessage.js
+++ Client/src/component/dashboard/Notification/NotificationMessage.js
@@ -1,15 +1,25 @@
 import React from 'react'
+const truncateContent = (content, wordLimit) => {
+    const words = content.split(' ');
+    if (words.length > wordLimit) {
+      return words.slice(0, wordLimit).join(' ') + '...';
+    }
+    return content;
+  };
 
 const NotificationMessage = ({ type, content, date, time, isSeen }) => {
+    const truncatedContent = truncateContent(content, 7);
     return (
-        <div className={isSeen?'row p-2 border-bottom': 'row p-2 text-white border-bottom'} style={{backgroundColor: isSeen?"":"grey"}}>
+        <div className={isSeen?'row p-2 border-bottom ': 'row p-2 text-white border-bottom'} style={{backgroundColor: isSeen?"":"grey", cursor:"pointer"}}>
             <div className='col-md-10'>
                 <div className='row'>
                     <div style={{ display: "flex" }}>
-                        <img src="https://github.com/mdo.png" alt="mdo" style={{ width: "10%" }} className="rounded-circle" />
+                        <div style={{ width: "10%" }}>
+                            <img src="https://github.com/mdo.png" alt="mdo" style={{ width: "100%" }} className="rounded-circle" />
+                        </div>
                         <div className='row mx-2'>
                         <h6><b>{type}!</b></h6>
-                        <p>{content}</p>
+                        <p>{truncatedContent}</p>
                         </div>
                     </div>
                 </div>
